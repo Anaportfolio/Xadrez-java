fde65575c0a44490060f46406e9cf7ed1ae7bb8c
Ana Beatriz <biamaiaribeiro@gmail.com>
2024-01-09 08:37:44 -0300

    Imprimindo os movimentos possíveis

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,21 +27,29 @@ public class ChessMatch {
 		return mat;
 	}
 
+	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+
 	// Movimento
 	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
 		validateTargetPositio(source, target);
-		Piece capturedPiece = makeMove(source,target);
+		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
+
 	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		return capturedPiece;
 	}
+
 	private void validateSourcePosition(Position position) {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("Não existe peça na posição origem");
@@ -50,12 +58,13 @@ public class ChessMatch {
 			throw new ChessException("Não existe movimentos possiveis para a peça escolhida");
 		}
 	}
-	
+
 	private void validateTargetPositio(Position source, Position target) {
 		if (!board.piece(source).possibleMove(target)) {
 			throw new ChessException("A peça escolhida não pode se mover para posição de destino");
 		}
 	}
+
 	// Vai receber as cordenadas do Xadrez
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
